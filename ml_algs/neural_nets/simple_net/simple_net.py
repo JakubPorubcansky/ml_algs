4c9a2dc71f1618f282f94776da0b7f075f41fe5d
JakubPorubcansky <jakub.porubcansky@gmail.com>
2024-07-09 09:21:20 +0200

    simple net modifications

--- ml_algs/neural_nets/simple_net/simple_net.py
+++ ml_algs/neural_nets/simple_net/simple_net.py
@@ -1,23 +1,29 @@
 import torch
 
 class SimpleNet:
-    def __init__(self, input_len: int, output_len: int, random_seed: int = 1):
+    def __init__(self, input_len: int, output_len: int, regularization_rate: float, 
+                 learning_rate: float, random_seed: int = 1):
         g = torch.Generator().manual_seed(random_seed)
         self.W = torch.randn(input_len, output_len, generator=g, requires_grad=True)
         self.W.grad = None
+        self.regularization_rate = regularization_rate
+        self.learning_rate = learning_rate
         self.losses = []
 
-    def train(self, xs, ys, n_epochs: int, learning_rate: float, regularization_rate: float):
+    def train(self, xs, ys, n_epochs: int):
         for _ in range(n_epochs):
             probs = self._forward(xs)
 
-            loss = -probs[torch.arange(len(ys)), ys].log().mean() + regularization_rate * (self.W**2).mean()
+            loss = self.calculate_loss(probs, ys)
             loss.backward()
             
             print(loss.item())
             self.losses.append(loss.item())
 
-            self.W.data += - learning_rate * self.W.grad
+            self.W.data += - self.learning_rate * self.W.grad
+
+    def calculate_loss(self, probs, ys):
+        return -probs[torch.arange(len(ys)), ys].log().mean() + self.regularization_rate * (self.W**2).mean() 
     
     def eval(self, xs):
         return self._forward(xs)
